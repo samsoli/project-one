d3415b5a85b10c68f64693fe147d2f6663bfc1ec
sama533 <sbit533@gmail.com>
2022-01-03 21:09:28 +0330

    new repo and add

---
+++ assets/js/custom-js.js
@@ -0,0 +1,13 @@
+$('.alert').alert();
+// $(function(){
+//     $('[data-toggle="tooltip"]').tooltip();
+// });
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();   
+  });
+
+  $(function(){
+    $('[data-toggle="popover"]').popover();   
+  });  
+
+  
